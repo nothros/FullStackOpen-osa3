f6e8faeff4b40b27f45244a291433da5cea85b4d
Erika Horros-Wallin <erika.horros@gmail.com>
2022-01-02 17:18:23 +0200

    Add backend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token("person", (req) => {
     const { body } = req;
   
